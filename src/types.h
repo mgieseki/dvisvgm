12bd79b50cb4a5be51cd91185a4b96fe69c7b246
Martin Gieseking <martin.gieseking@uos.de>
2009-03-09 13:09:38 +0100

    minor changes to homepage and .hgignore

--- src/types.h
+++ src/types.h
@@ -24,23 +24,23 @@
 #define TYPES_H
 
 namespace internal {
-	template<unsigned BYTES>
-	class ERROR_inttype_not_available
-	{
-		ERROR_inttype_not_available();
-	};
+   template<unsigned BYTES>
+   class ERROR_inttype_not_available
+   {
+      ERROR_inttype_not_available();
+   };
 
-	template<bool FIRST, typename A, typename B> 
-	struct select
-	{
-		typedef A T;
-	};
+   template<bool FIRST, typename A, typename B> 
+   struct select
+   {
+      typedef A T;
+   };
 
-	template<typename A, typename B> 
-	struct select<false, A, B>
-	{
-		typedef B T;
-	};	
+   template<typename A, typename B> 
+   struct select<false, A, B>
+   {
+      typedef B T;
+   }; 
 }
 
 
@@ -48,12 +48,12 @@ namespace internal {
 template<unsigned BYTES, bool SIGNED>
 struct int_t
 {
-	typedef typename internal::select<sizeof(signed char)       == BYTES, signed char,
-			  typename internal::select<sizeof(signed short)      == BYTES, signed short,
-			  typename internal::select<sizeof(signed int)        == BYTES, signed int,
-			  typename internal::select<sizeof(signed long)       == BYTES, signed long,
-			  typename internal::select<sizeof(signed long long)  == BYTES, signed long long,
-			  internal::ERROR_inttype_not_available<BYTES> >::T>::T>::T>::T>::T T;
+   typedef typename internal::select<sizeof(signed char)       == BYTES, signed char,
+           typename internal::select<sizeof(signed short)      == BYTES, signed short,
+           typename internal::select<sizeof(signed int)        == BYTES, signed int,
+           typename internal::select<sizeof(signed long)       == BYTES, signed long,
+           typename internal::select<sizeof(signed long long)  == BYTES, signed long long,
+           internal::ERROR_inttype_not_available<BYTES> >::T>::T>::T>::T>::T T;
 };
 
 
@@ -61,24 +61,24 @@ struct int_t
 template<unsigned BYTES>
 struct int_t<BYTES, false>
 {
-	typedef typename internal::select<sizeof(unsigned char)      == BYTES, unsigned char,
-		     typename internal::select<sizeof(unsigned short)     == BYTES, unsigned short,
-			  typename internal::select<sizeof(unsigned int)       == BYTES, unsigned int,
-			  typename internal::select<sizeof(unsigned long)      == BYTES, unsigned long,
-			  typename internal::select<sizeof(unsigned long long) == BYTES, unsigned long long,
-			  internal::ERROR_inttype_not_available<BYTES> >::T>::T>::T>::T>::T T;
+   typedef typename internal::select<sizeof(unsigned char)      == BYTES, unsigned char,
+           typename internal::select<sizeof(unsigned short)     == BYTES, unsigned short,
+           typename internal::select<sizeof(unsigned int)       == BYTES, unsigned int,
+           typename internal::select<sizeof(unsigned long)      == BYTES, unsigned long,
+           typename internal::select<sizeof(unsigned long long) == BYTES, unsigned long long,
+           internal::ERROR_inttype_not_available<BYTES> >::T>::T>::T>::T>::T T;
 };
 
 
 // Machine independent definition of sized integer types
-typedef int_t<1, true>::T	Int8;
-typedef int_t<2, true>::T	Int16;
-typedef int_t<4, true>::T	Int32;
-typedef int_t<8, true>::T	Int64;
-typedef int_t<1, false>::T	UInt8;
-typedef int_t<2, false>::T	UInt16;
-typedef int_t<4, false>::T	UInt32;
-typedef int_t<8, false>::T	UInt64;
+typedef int_t<1, true>::T  Int8;
+typedef int_t<2, true>::T  Int16;
+typedef int_t<4, true>::T  Int32;
+typedef int_t<8, true>::T  Int64;
+typedef int_t<1, false>::T UInt8;
+typedef int_t<2, false>::T UInt16;
+typedef int_t<4, false>::T UInt32;
+typedef int_t<8, false>::T UInt64;
 
 typedef Int32  FixWord;
 typedef UInt32 ScaledInt;
