44a935c59dda81ff670cc9ce6b7aaeb270b1c1ec
Martin Gieseking <martin.gieseking@uos.de>
2009-06-01 22:08:48 +0200

    transplanted changes to InputBuffer from branch 'pages'

--- src/InputBuffer.cpp
+++ src/InputBuffer.cpp
@@ -20,35 +20,186 @@
 ** Boston, MA 02110-1301, USA.                                        **
 ***********************************************************************/
 
+#include <cmath>
 #include <cstring>
 #include "InputBuffer.h"
+#include "debug.h"
 
 using namespace std;
 
-int InputBuffer::getInt () {
-	int ret=0;
-	int fac=1;
-	skipSpace();
-	if (peek() == '-') {
-		fac = -1;
+
+/** Skips n characters. */
+void InputBuffer::skip (unsigned n) {
+	while (n-- > 0) 
 		get();
-	}
-	while (isdigit(peek()))
-		ret = ret*10 + (get()-'0');
-	return fac*ret;
 }
 
 
-double InputBuffer::getDouble () {
-	double ret=getInt();
-	if (peek() == '.') {
+/** Moves the buffer pointer to the next non-space character. A following call
+ *  of get() returns this character. */
+void InputBuffer::skipSpace () {
+	while (isspace(peek()))
 		get();
-		double frac=0;
-		for (double u=10; isdigit(peek()); u*=10) 
-			frac += (get()-'0')/u;
-		ret += frac;
+}
+
+
+/** Tries to find a given string and skips all characters preceding that string. 
+ *  @param[in] s string to look for (must not be longer than the maximal buffer size)
+ *  @param[in] consume if true, the buffer pointer is moved to the first charater following string s
+ *  @return true if s was found */
+bool InputBuffer::skipUntil (const char *s, bool consume) {
+	bool found;
+	while (!eof() && !(found = check(s, consume)))
+		get();
+	return found;
+}
+
+
+/** Checks if the next characters to be read match a given string. 
+ *  @param[in] s string to be matched 
+ *  @param[in] consume if true, the characters of the matched string are skipped 
+ *  @return true if s matches */
+bool InputBuffer::check (const char *s, bool consume) {
+	size_t count = 0;
+	for (const char *p=s; *p; p++) {
+		if (peek(count++) != *p)
+			return false;
 	}
-	return ret;
+	if (consume)
+		skip(count);
+	return true;
+}
+
+
+int InputBuffer::compare (const char *s, bool consume) {
+	size_t count = 0;
+	for (const char *p=s; *p; p++) {
+		int c = peek(count++);
+		if (c != *p)
+			return *p < c ? -1 : 1;
+	}
+	if (consume)
+		skip(count);
+	return 0;
+}
+
+
+/** Reads an integer from the buffer. All characters that are part of
+ *  the read integer constant are skipped. If this function returns false,
+ *  the buffer pointer points to the same position as before the function call.
+ *  @param[out] val contains the read integer value on success
+ *  @param[in] accept_sign if false, only positive integers (without sign) are accepted 
+ *  @return true if integer could be read */
+bool InputBuffer::parseInt (int &val, bool accept_sign) {
+	val = 0;
+	int fac=1;
+	char sign;    // explicitly given sign
+	if (accept_sign && ((sign = peek()) == '+' || sign == '-')) {
+		if (isdigit(peek(1))) {
+			get();  // skip sign
+			if (sign == '-')
+				fac = -1;
+		}
+		else
+			return false;
+	}
+	else if (!isdigit(peek()))
+		return false;
+	
+	while (isdigit(peek()))
+		val = val*10 + (get()-'0');
+	val *= fac;
+	return true;
+}
+
+
+bool InputBuffer::parseInt (int base, int &val) {
+	if (base < 2 || base > 32)
+		return false;
+
+	const char *digits = "0123456789abcdefghijklmnopqrstuvwxyz";
+	const char maxdigit = digits[base-1];
+	char c;
+	if (!isalnum(c = tolower(peek())) || c > maxdigit)
+		return false;
+
+	val = 0;
+	while (isalnum(c = tolower(peek())) && c <= maxdigit) {
+		get();
+		int digit = c - (c <= '9' ? '0' : 'a'-10);
+		val = val*base + digit;
+	}
+	return true;	
+}
+
+
+/** Reads a double from the buffer. All characters that are part of
+ *  the read double constant are skipped. If this function returns false,
+ *  the buffer pointer points to the same position as before the function call.
+ *  @param[out] val contains the read double value on success
+ *  @return number details: 0=no number, 'i'=integer, 'f'=floating point number */
+char InputBuffer::parseDouble (double &val) {
+	int fac=1;
+	int int_part=0;
+	bool is_float = false;
+	if (parseInt(int_part)) { // match [+-]?[0-9]+\.?
+		if (peek() == '.') {
+			get();
+			is_float = true;
+		}
+		if (int_part < 0) {
+			fac = -1;
+			int_part = -int_part;
+		}
+	}
+	else {  // match [+-]?\.
+		char sign;   // explicitly given sign
+		if ((sign = peek()) == '+' || sign == '-') { // match [+-]?\.[0-9]
+			if (peek(1) != '.' || !isdigit(peek(2)))
+				return 0;
+			if (sign == '-')
+				fac = -1;
+			skip(2);  // skip sign and dot
+		}
+		else if (peek() == '.' && isdigit(peek(1)))
+			get();
+		else
+			return 0;
+		is_float = true;
+	}
+	// parse fractional part
+	double frac_part=0.0;
+	for (double u=10; isdigit(peek()); u*=10)
+		frac_part += (get()-'0')/u;
+	val = (int_part + frac_part) * fac;
+	// parse exponent
+	char c;
+	if (tolower(peek()) == 'e' && (isdigit(c=peek(1)) || ((c == '+' || c == '-') && isdigit(peek(2))))) {
+		get(); // skip 'e'
+		int exp;
+		parseInt(exp);
+		val *= pow10(exp);
+		is_float = true;
+	}
+	return is_float ? 'f' : 'i';
+}
+
+
+/** Reads an integer value from the buffer. If no valid integer constant
+ *  could be found at the current position 0 is returned. */
+int InputBuffer::getInt () {
+	skipSpace();
+	int val;
+	return parseInt(val) ? val : 0;
+}
+
+
+/** Reads an double value from the buffer. If no valid double constant
+ *  could be found at the current position 0 is returned. */
+double InputBuffer::getDouble () {
+	skipSpace();
+	double val;
+	return parseDouble(val) ? val : 0.0;
 }
 
 
@@ -59,6 +210,7 @@ string InputBuffer::getWord () {
 		ret += get();
 	return ret;
 }
+
 
 char InputBuffer::getPunct () {
 	skipSpace();
@@ -78,11 +230,11 @@ string InputBuffer::getString () {
 
 //////////////////////////////////////////
 
-StreamInputBuffer::StreamInputBuffer (istream &is, int bufsize)
-	: _is(is), _bufsize(bufsize), _buf1(new char[_bufsize]), _buf2(new char[_bufsize]), _bufptr(_buf1), _line(1), _col(1)
+StreamInputBuffer::StreamInputBuffer (istream &is, size_t bufsize)
+	: _is(is), _bufsize(bufsize), _buf1(new UInt8[_bufsize]), _buf2(new UInt8[_bufsize]), _bufptr(_buf1)
 {
-	fillBuffer(_buf1);
-	fillBuffer(_buf2);
+	_size1 = fillBuffer(_buf1);
+	_size2 = fillBuffer(_buf2);
 }
 
 
@@ -92,80 +244,49 @@ StreamInputBuffer::~StreamInputBuffer () {
 }
 
 
-char StreamInputBuffer::get () {
-	if (*_bufptr == 0) {
-		if (*_buf2 == 0)
-			return 0;
+int StreamInputBuffer::get () {
+	if (pos() == _size1) {
+		if (_size2 == 0)
+			return -1;
 		swap(_buf1, _buf2);
+		_size1 = _size2;
 		_bufptr = _buf1;
-		fillBuffer(_buf2);
+		_size2 = fillBuffer(_buf2);
 	}
-	char c = *_bufptr++;
-	if (c == '\n') {
-		_line++;
-		_col = 1;
-	}
-	else
-		_col++;
+	UInt8 c = *_bufptr++;
 	return c;
 }
 
 
-char StreamInputBuffer::peek () const {
-	if (*_bufptr)
+/** Returns the next character to be read without skipping it. 
+ *  Same as peek(0). */
+int StreamInputBuffer::peek () const {
+	if (pos() < _size1)
 		return *_bufptr;
-	return *_buf2;
+	return _size2 > 0 ? *_buf2 : -1;
 }
 
 
-char StreamInputBuffer::peek (int n) const {
-	if (pos()+n < _bufsize-1)
+/** Returns the n-th next character without skipping it. */
+int StreamInputBuffer::peek (size_t n) const {
+	if (pos()+n < _size1)
 		return *(_bufptr+n);
-	return *(_buf2 + pos()+n-_bufsize+1);
+	if (pos()+n < _size1+_size2)
+		return *(_buf2 + pos()+n-_size1);
+	return -1;
 }
 
 
-bool StreamInputBuffer::check (const char *s, bool consume) {
-	const char *bp = _bufptr;
-	for (const char *p=s; *p; p++) {
-		if (*bp == 0)
-			bp = _buf2;
-		if (*bp++ != *p)
-			return false;
-	}
-	if (consume)
-		skip(strlen(s));
-	return true;
-}
-
-
-void StreamInputBuffer::skip (unsigned n) {
-	while (n-- > 0) 
-		get();
-}
-
-
-bool StreamInputBuffer::skipUntil (const char *s, bool consume) {
-	bool found;
-	while (!eof() && !(found = check(s, consume)))
-		get();
-	return found;
-}
-
-
-void StreamInputBuffer::skipSpace () {
-	while (isspace(peek()))
-		get();
-}
-
-
-void StreamInputBuffer::fillBuffer (char *buf) {
-	streamsize ofs=0;
+/** Fills the buffer by reading a sequence of characters from the assigned
+ *  input stream. 
+ *  @param[in] buf pointer to character buffer to be filled 
+ *  @return number of characters read */
+int StreamInputBuffer::fillBuffer (UInt8 *buf) {
 	if (_is && !_is.eof()) {
-		_is.read(buf, _bufsize-1);
-		ofs = _is.gcount();
+		_is.read((char*)buf, _bufsize);
+		return  _is.gcount();
 	}
-	memset(buf+ofs, 0, _bufsize-ofs);
+	return 0;
 }
 
 
@@ -179,37 +300,109 @@ bool StringInputBuffer::check (const char *s, bool consume) {
 	return ret;
 }
 
-		
-bool StringInputBuffer::skipUntil (const char *s, bool consume) {
-	bool found;
-	while (!eof() && !(found = check(s, consume)))
-		get();
-	return found;
-}
+///////////////////////////////////////////////
 
+bool CharInputBuffer::check (const char *s, bool consume) {
+	const char *p1=s, *p2=_pos; 
+	size_t n = strlen(s);
+	if (n < _size)
+		return false;
 
-void StringInputBuffer::skipSpace () {
-	while (isspace(peek()))
-		get();
-}
-
-
-#if 0
-#include <fstream>
-int main (int argc, char **argv) {
-	if (argc > 1) {
-		ifstream ifs(argv[1]);
-		StreamInputBuffer b(ifs);
-		int count = 0;
-		
-		bool ok = true;		
-		while (ok) {
-			ok = b.skipUntil("<!--", true);
-			while (ok && !b.check("-->", true))
-				cout << b.get();
-		}
+	while (n-- > 0 && *p1 == *p2) {
+		p1++;
+		p2++;
 	}
-	return 0;
+	if (consume && n == 0)
+		_pos += strlen(s);
+	return n == 0;
 }
 
-#endif
+///////////////////////////////////////////////
+
+SplittedCharInputBuffer::SplittedCharInputBuffer (const char *buf1, size_t s1, const char *buf2, size_t s2) {
+	_buf[0] = buf1;
+	_buf[1] = buf2;
+	_size[0] = buf1 ? s1 : 0;
+  	_size[1] = buf2 ? s2 : 0;
+	_index = _size[0] ? 0 : 1;
+}
+
+
+int SplittedCharInputBuffer::get () {
+	if (_size[_index] == 0)
+		return -1;
+	int ret = *_buf[_index]++;
+	_size[_index]--;
+	if (_index == 0 && _size[0] == 0)
+		_index++;
+	return ret;
+}
+
+
+int SplittedCharInputBuffer::peek () const {
+	return _size[_index] ? *_buf[_index] : -1;
+}
+
+
+int SplittedCharInputBuffer::peek (size_t n) const {
+	if (n < _size[_index])
+		return _buf[_index][n];
+	n -= _size[_index];
+	if (_index == 0 && n < _size[1])
+		return _buf[1][n];
+	return -1;
+}
+
+
+void SplittedCharInputBuffer::skip (size_t n) {
+	if (n < _size[_index]) {
+		_size[_index] -= n;
+		_buf[_index] += n;
+	}
+	else if (_index == 0) {
+		n -= _size[_index];
+		_index++;
+		_size[1] -= n;
+		_buf[1] += n;
+	}
+	else
+		_size[1] = 0;
+}
+
+
+bool SplittedCharInputBuffer::check (const char *str, bool consume) {
+	const char *p1 = _buf[_index];
+	const char *p2 = str;
+	int index = _index;
+	size_t size = _size[index];
+	while (size > 0 && *p2 && *p1 == *p2) {
+		p1++;
+		p2++;
+		if (--size == 0 && index == 0) {
+			size = _size[1];
+			p1 = _buf[1];
+			index++;
+		}		
+	}
+	if (consume && *p2 == 0) {
+		_index = index;
+		_size[index] = size;
+		_buf[index] = p1;
+	}
+	return *p2 == 0;
+}
+///////////////////////////////////////////////
+
+
+int TextStreamInputBuffer::get () {
+	int c = StreamInputBuffer::get();
+	if (c == '\n') {
+		_line++;
+		_col = 1;
+	}
+	else
+		_col++;
+	return c;
+}
+
+
--- src/InputBuffer.h
+++ src/InputBuffer.h
@@ -27,79 +27,137 @@
 #include <istream>
 #include <string>
 #include <ostream>
-
+#include "types.h"
 
 struct InputBuffer
 {
 	virtual ~InputBuffer () {}
-	virtual int pos () const =0;
-	virtual char get () =0;
-	virtual char peek () const =0;
-	virtual char peek (int n) const =0;
+	virtual int get () =0;
+	virtual int peek () const =0;
+	virtual int peek (size_t n) const =0;
 	virtual bool check (char c) const {return peek() == c;}
-	virtual bool check (const char *s, bool consume=true) =0;
-	virtual void skip (unsigned n) =0;
-	virtual bool skipUntil (const char *s, bool consume=true) =0;
-	virtual void skipSpace () =0;
+	virtual bool check (const char *s, bool consume=true);
+	virtual int compare (const char *s, bool consume=true);
+	virtual void skip (size_t n);
+	virtual bool skipUntil (const char *s, bool consume=true);
+	virtual void skipSpace ();
 	virtual bool eof () const =0;
 	virtual int getInt ();
+	virtual bool parseInt (int &val, bool accept_sign=true);
+	virtual bool parseInt (int base, int &val);
+	virtual char parseDouble (double &val);
 	virtual double getDouble ();
 	virtual std::string getWord ();
 	virtual char getPunct ();
 	virtual std::string getString ();
-
-//	void write (std::ostream &os) const;
+	virtual operator bool () const {return !eof();}
 };
 
 
 class StreamInputBuffer : public InputBuffer
 {
 	public:
-		StreamInputBuffer (std::istream &is, int bufsize=1024);
+		StreamInputBuffer (std::istream &is, size_t bufsize=1024);
 		~StreamInputBuffer ();
-		int pos () const {return _bufptr-_buf1;}
-		int line () const {return _line;}
-		int col () const  {return _col;}
-		char get ();
-		char peek () const;
-		char peek (int n) const;
-		bool check (char c) const {return peek() == c;}
-		bool check (const char *s, bool consume=true);
-		void skip (unsigned n);
-		bool skipUntil (const char *s, bool consume=true);
-		void skipSpace ();
-		bool eof () const {return *_bufptr == 0 && *_buf2 == 0;}
+		int get ();
+		int peek () const;
+		int peek (size_t n) const;
+		bool eof () const {return pos() == _size1 && _size2 == 0;}
 
 	protected:
-		void fillBuffer (char *buf);
+		int fillBuffer (UInt8 *buf);
+		size_t pos () const  {return _bufptr-_buf1;}
 
 	private:
 		std::istream &_is;
-		const int _bufsize;
-		char *_buf1;
-		char *_buf2;
-		char *_bufptr;
-		int _line, _col;
+		const size_t _bufsize;  ///< maximal number of bytes each buffer can hold
+		UInt8 *_buf1;        ///< pointer to first buffer
+		UInt8 *_buf2;        ///< pointer to second buffer
+		size_t _size1;          ///< number of bytes in buffer 1
+		size_t _size2;          ///< number of bytes in buffer 2
+		UInt8 *_bufptr;      ///< pointer to next byte to read
 };
 
 
 class StringInputBuffer : public InputBuffer
 {
 	public:
-		StringInputBuffer (std::string &str);
+		StringInputBuffer (std::string &str) : _str(str), _pos(0) {}
 		int pos () const        {return _pos;}
-		char get ()             {return _pos < _str.length() ? _str[_pos++] : 0;}
-		char peek () const      {return _pos < _str.length() ? _str[_pos] : 0;}
-		char peek (int n) const {return _pos+n < _str.length() ? _str[_pos+n] : 0;} 
-		bool check (const char *s, bool consume=true);
+		int get ()              {return _pos < _str.length() ? _str[_pos++] : -1;}
+		int peek () const       {return _pos < _str.length() ? _str[_pos] : -1;}
+		int peek (size_t n) const {return _pos+n < _str.length() ? _str[_pos+n] : -1;} 
 		void skip (size_t n)    {_pos += std::min(n, _str.length()-_pos);}
-		bool skipUntil (const char *s, bool consume=true);
-		void skipSpace ();
 		bool eof () const       {return _pos >= _str.length();}
+		bool check (const char *s, bool consume=true);
 
 	private:
 		std::string &_str;
 		size_t _pos;
+};
+
+
+class CharInputBuffer : public InputBuffer
+{
+	public:
+		CharInputBuffer (const char *buf, size_t size) : _pos(buf), _size(buf ? size : 0) {}
+		
+		int get () {
+			if (_size <= 0) 
+				return -1;
+			else {
+				_size--;
+				return *_pos++;
+			}
+		}
+
+		int peek () const         {return _size > 0 ? *_pos : -1;}
+		int peek (size_t n) const {return _size >= (size_t)n ? _pos[n] : -1;}
+
+		void skip (size_t n) {
+			if ((n = std::min(n, _size)) >= _size) {
+				_pos += n;
+				_size -= n;
+			}			
+		}
+
+		bool eof () const      {return _size <= 0;}
+		bool check (const char *s, bool consume=true);
+	
+	private:
+		const char *_pos;
+		size_t _size;
+};
+
+
+class SplittedCharInputBuffer : public InputBuffer
+{
+	public:
+		SplittedCharInputBuffer (const char *buf1, size_t s1, const char *buf2, size_t s2);
+		int get ();
+		int peek () const;
+		int peek (size_t n) const;
+		void skip (size_t n); 
+		bool eof () const {return _size[_index] == 0;}
+		bool check (const char *s, bool consume=true);
+
+	private:
+		const char *_buf[2];
+		size_t _size[2];
+		int _index;
+};
+
+
+class TextStreamInputBuffer : public StreamInputBuffer
+{
+	public:
+		TextStreamInputBuffer (std::istream &is) : StreamInputBuffer(is), _line(1), _col(1) {}
+		int get ();
+		int line () const {return _line;}
+		int col () const {return _col;}
+
+	private:
+		int _line, _col;
 };
 
 #endif
