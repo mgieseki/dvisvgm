4db2de1a5dd0a8938e8ff333f30b1ce23e621dd0
Martin Gieseking <martin.gieseking@uos.de>
2009-07-22 22:34:35 +0200

    fixed reintroduced bug in TPIC handler

--- src/TpicSpecialHandler.cpp
+++ src/TpicSpecialHandler.cpp
@@ -225,6 +225,7 @@ void TpicSpecialHandler::drawArc (double cx, double cy, double rx, double ry, do
 		actions->appendToPage(elem);
 		actions->bbox().embed(BoundingBox(cx-rx, cy-ry, cx+rx, cy+ry));
 	}
+	reset();
 }
 
 
