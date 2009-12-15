0447a25ccb19b725211bc7b36191aa261e358730
Martin Gieseking <mgieseki@users.sourceforge.net>
2009-12-15 18:26:10 +0100

    appied get_time patch sent by Peter Breitenlohner

--- src/dvisvgm.cpp
+++ src/dvisvgm.cpp
@@ -23,7 +23,6 @@
 #include <iostream>
 #include <sstream>
 #include <string>
-#include <sys/timeb.h>
 #include <time.h>
 #include "gzstream.h"
 #include "CommandLine.h"
@@ -41,6 +40,12 @@
 
 #ifdef HAVE_CONFIG_H
 #include "config.h"
+#endif
+
+#if defined (HAVE_SYS_TIME_H)
+#include <sys/time.h>
+#elif defined (HAVE_SYS_TIMEB_H)
+#include <sys/timeb.h>
 #endif
 
 using namespace std;
@@ -103,9 +108,18 @@ static string remove_suffix (string fname) {
 
 /** Returns timestamp (wall time) in seconds. */
 static double get_time () {
+#if defined (HAVE_SYS_TIME_H)
+	struct timeval tv;
+	gettimeofday(&tv, NULL);
+	return tv.tv_sec + tv.tv_usec/1000000.0;
+#elif defined (HAVE_SYS_TIMEB_H)
 	struct timeb tb;
 	ftime(&tb);
 	return tb.time + tb.millitm/1000.0;
+#else
+	time_t myclock = time((time_t*)NULL);
+	return myclock;
+#endif
 }
 
 
