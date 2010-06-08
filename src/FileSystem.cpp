d9a943ecb69082667c3b528a010ad40862297da5
Martin Gieseking <mgieseki@users.sourceforge.net>
2010-06-08 11:34:54 +0200

    added class FilePath

--- src/FileSystem.cpp
+++ src/FileSystem.cpp
@@ -4,7 +4,7 @@
 ** This file is part of dvisvgm -- the DVI to SVG converter             **
 ** Copyright (C) 2005-2010 Martin Gieseking <martin.gieseking@uos.de>   **
 **                                                                      **
-** This program is free software; you can redistribute it and/or        ** 
+** This program is free software; you can redistribute it and/or        **
 ** modify it under the terms of the GNU General Public License as       **
 ** published by the Free Software Foundation; either version 3 of       **
 ** the License, or (at your option) any later version.                  **
@@ -15,7 +15,7 @@
 ** GNU General Public License for more details.                         **
 **                                                                      **
 ** You should have received a copy of the GNU General Public License    **
-** along with this program; if not, see <http://www.gnu.org/licenses/>. ** 
+** along with this program; if not, see <http://www.gnu.org/licenses/>. **
 *************************************************************************/
 
 #include <cstdlib>
@@ -23,7 +23,11 @@
 #include <fstream>
 #include "FileSystem.h"
 
-#ifdef __GNUC__
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#ifdef HAVE_UNISTD_H
 #include <unistd.h>
 #endif
 
---
+++ tests/FilePathTest.cpp
@@ -0,0 +1,89 @@
+/*************************************************************************
+** FilePathTest.cpp                                                     **
+**                                                                      **
+** This file is part of dvisvgm -- the DVI to SVG converter             **
+** Copyright (C) 2005-2010 Martin Gieseking <martin.gieseking@uos.de>   **
+**                                                                      **
+** This program is free software; you can redistribute it and/or        **
+** modify it under the terms of the GNU General Public License as       **
+** published by the Free Software Foundation; either version 3 of       **
+** the License, or (at your option) any later version.                  **
+**                                                                      **
+** This program is distributed in the hope that it will be useful, but  **
+** WITHOUT ANY WARRANTY; without even the implied warranty of           **
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the         **
+** GNU General Public License for more details.                         **
+**                                                                      **
+** You should have received a copy of the GNU General Public License    **
+** along with this program; if not, see <http://www.gnu.org/licenses/>. **
+*************************************************************************/
+
+#include <gtest/gtest.h>
+#include <string>
+#include "FilePath.h"
+#include "FileSystem.h"
+
+using namespace std;
+
+
+TEST(FilePathTest, dir1) {
+	FilePath fp("a/b/c/d", false, "/");
+	ASSERT_EQ(fp.absolute(), "/a/b/c/d");
+	ASSERT_EQ(fp.relative("/"), "a/b/c/d");
+	ASSERT_EQ(fp.relative("/a/b"), "c/d");
+	ASSERT_EQ(fp.relative("/a/b/c"), "d");
+	ASSERT_EQ(fp.relative("/a/b/c/d"), ".");
+	ASSERT_EQ(fp.relative("/a/b/x"), "../c/d");
+	ASSERT_EQ(fp.relative("/a/b/x/y"), "../../c/d");
+}
+
+
+TEST(FilePathTest, dir2) {
+	FilePath fp("a/b/c/d", false, "/x/y");
+	ASSERT_EQ(fp.absolute(), "/x/y/a/b/c/d");
+	ASSERT_EQ(fp.relative("/"), "x/y/a/b/c/d");
+	ASSERT_EQ(fp.relative("/x/y/a/b"), "c/d");
+	ASSERT_EQ(fp.relative("/x/y/a/b/c"), "d");
+	ASSERT_EQ(fp.relative("/x/y/a/b/c/d"), ".");
+	ASSERT_EQ(fp.relative("/x/y/a/b/x"), "../c/d");
+	ASSERT_EQ(fp.relative("/x/y/a/b/x/y"), "../../c/d");
+}
+
+
+TEST(FilePathTest, file1) {
+	FilePath fp("a/b/c/d/f.ext", true, "/");
+	ASSERT_EQ(fp.absolute(), "/a/b/c/d/f.ext");
+	ASSERT_EQ(fp.relative("/"), "a/b/c/d/f.ext");
+	ASSERT_EQ(fp.relative("/a/b"), "c/d/f.ext");
+	ASSERT_EQ(fp.relative("/a/b/c"), "d/f.ext");
+	ASSERT_EQ(fp.relative("/a/b/c/d"), "f.ext");
+	ASSERT_EQ(fp.relative("/a/b/x"), "../c/d/f.ext");
+	ASSERT_EQ(fp.relative("/a/b/x/y"), "../../c/d/f.ext");
+	ASSERT_EQ(fp.basename(), "f");
+	ASSERT_EQ(fp.suffix(), "ext");
+	fp.suffix("new");
+	ASSERT_EQ(fp.suffix(), "new");
+	ASSERT_EQ(fp.relative("/a/b/x/y"), "../../c/d/f.new");
+}
+
+
+TEST(FilePathTest, file2) {
+	FilePath fp("/f.ext", true, "/");
+	ASSERT_EQ(fp.absolute(), "/f.ext");
+	ASSERT_EQ(fp.relative("/a/b"), "../../f.ext");
+}
+
+
+TEST(FilePathTest, autodetect) {
+	FilePath fp1("FilePathTest.cpp");
+	ASSERT_TRUE(fp1.isFile());
+	ASSERT_FALSE(fp1.empty());
+	string cwd = FileSystem::getcwd();
+	ASSERT_EQ(fp1.absolute(), cwd + "/FilePathTest.cpp");
+
+	FilePath fp2("");
+	ASSERT_FALSE(fp2.isFile());
+	ASSERT_FALSE(fp2.empty());
+	ASSERT_EQ(fp2.absolute(), FileSystem::getcwd());
+}
+
