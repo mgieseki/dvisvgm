deeb7bd21f9c70df1c78a5250a4283e7ad1f3d62
Martin Gieseking <mgieseki@users.sourceforge.net>
2010-07-13 11:03:01 +0200

    added missing namespace prefix

--- src/InputBuffer.h
+++ src/InputBuffer.h
@@ -4,7 +4,7 @@
 ** This file is part of dvisvgm -- the DVI to SVG converter             **
 ** Copyright (C) 2005-2010 Martin Gieseking <martin.gieseking@uos.de>   **
 **                                                                      **
-** This program is free software; you can redistribute it and/or        ** 
+** This program is free software; you can redistribute it and/or        **
 ** modify it under the terms of the GNU General Public License as       **
 ** published by the Free Software Foundation; either version 3 of       **
 ** the License, or (at your option) any later version.                  **
@@ -15,7 +15,7 @@
 ** GNU General Public License for more details.                         **
 **                                                                      **
 ** You should have received a copy of the GNU General Public License    **
-** along with this program; if not, see <http://www.gnu.org/licenses/>. ** 
+** along with this program; if not, see <http://www.gnu.org/licenses/>. **
 *************************************************************************/
 
 #ifndef INPUTBUFFER_H
@@ -101,7 +101,7 @@ class CharInputBuffer : public InputBuffer
 			_size = size;
 		}
 
-		void assign (const char *buf) {assign(buf, strlen(buf));}
+		void assign (const char *buf) {assign(buf, std::strlen(buf));}
 		int peek () const             {return _size > 0 ? *_pos : -1;}
 		int peek (unsigned n) const   {return _size >= n ? _pos[n] : -1;}
 		bool eof () const             {return _size <= 0;}
