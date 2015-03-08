3791c5c95ca1edcc60c71d9b70e46dcfd55be76b
Martin Gieseking <martin.gieseking@uos.de>
2015-03-08 15:29:21 +0100

    fixed UTF-8 encoding of 0xfffe and 0xffff; added UnicodeTest

--- tests/UnicodeTest.cpp
+++ tests/UnicodeTest.cpp
@@ -1,5 +1,5 @@
 /*************************************************************************
-** Unicode.cpp                                                          **
+** UnicodeTest.cpp                                                      **
 **                                                                      **
 ** This file is part of dvisvgm -- the DVI to SVG converter             **
 ** Copyright (C) 2005-2015 Martin Gieseking <martin.gieseking@uos.de>   **
@@ -18,58 +18,34 @@
 ** along with this program; if not, see <http://www.gnu.org/licenses/>. **
 *************************************************************************/
 
-#include <cstddef>
+#include <gtest/gtest.h>
 #include "Unicode.h"
 
 using namespace std;
 
 
-/** Returns true if c is a valid unicode point in XML documents.
- *  XML version 1.0 doesn't allow various unicode character references
- *  (&#1; for example). */
-bool Unicode::isValidCodepoint (UInt32 c) {
-	if ((c & 0xffff) == 0xfffe || (c & 0xffff) == 0xffff)
-		return false;
-
-	UInt32 ranges[] = {
-		0x0000, 0x0020,
-		0x007f, 0x0084,
-		0x0086, 0x009f,
-		0x202a, 0x202e,  // bidi control characters
-		0xd800, 0xdfff,
-		0xfdd0, 0xfdef,
-	};
-	for (size_t i=0; i < sizeof(ranges)/sizeof(UInt32)/2; i++)
-		if (c >= ranges[2*i] && c <= ranges[2*i+1])
-			return false;
-	return true;
+TEST(TypesTest, isValidCodepoint) {
+	for (UInt32 i=0; i <= 0x20; i++)
+		EXPECT_FALSE(Unicode::isValidCodepoint(i));
+	for (UInt32 i=0x21; i <= 0x7e; i++)
+		EXPECT_TRUE(Unicode::isValidCodepoint(i));
+	EXPECT_FALSE(Unicode::isValidCodepoint(0xfffe));
+	EXPECT_FALSE(Unicode::isValidCodepoint(0xffff));
+	EXPECT_TRUE(Unicode::isValidCodepoint(0x10000));
 }
 
 
-/** Converts a unicode value to a UTF-8 byte sequence.
- *  @param[in] c character code
- *  @return  utf8 seqence consisting of 1-4 bytes */
-string Unicode::utf8 (Int32 c) {
-	string utf8;
-	if (c >= 0) {
-		if (c < 0x80)
-			utf8 += c;
-		else if (c < 0x800) {
-			utf8 += 0xC0 + (c >> 6);
-			utf8 += 0x80 + (c & 0x3F);
-		}
-		else if (c < 0x10000) {
-			utf8 += 0xE0 + (c >> 12);
-			utf8 += 0x80 + ((c >> 6) & 0x3F);
-			utf8 += 0x80 + (c & 0x3F);
-		}
-		else if (c < 0x110000) {
-			utf8 += 0xF0 + (c >> 18);
-			utf8 += 0x80 + ((c >> 12) & 0x3F);
-			utf8 += 0x80 + ((c >> 6) & 0x3F);
-			utf8 += 0x80 + (c & 0x3F);
-		}
-		// UTF-8 does not support codepoints >= 0x110000
-	}
-	return utf8;
+TEST(TypesTest, utf8) {
+	EXPECT_EQ(Unicode::utf8(0x1), string("\x01"));
+	EXPECT_EQ(Unicode::utf8(0x47), string("\x47"));
+	EXPECT_EQ(Unicode::utf8(0x7f), string("\x7f"));
+	EXPECT_EQ(Unicode::utf8(0x80), string("\xc2\x80"));
+	EXPECT_EQ(Unicode::utf8(0x07ff), string("\xdf\xbf"));
+	EXPECT_EQ(Unicode::utf8(0x0800), string("\xe0\xa0\x80"));
+	EXPECT_EQ(Unicode::utf8(0x7fff), string("\xe7\xbf\xbf"));
+	EXPECT_EQ(Unicode::utf8(0xfffe), string("\xef\xbf\xbe"));
+	EXPECT_EQ(Unicode::utf8(0xffff), string("\xef\xbf\xbf"));
+	EXPECT_EQ(Unicode::utf8(0x10000), string("\xf0\x90\x80\x80"));
+	EXPECT_EQ(Unicode::utf8(0x10ffff), string("\xf4\x8f\xbf\xbf"));
+	EXPECT_TRUE(Unicode::utf8(0x110000).empty());
 }
