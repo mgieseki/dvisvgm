a7a3272b4381fef47370d0933d548254f9deca9a
Martin Gieseking <martin.gieseking@uos.de>
2015-12-26 22:37:36 +0100

    lookup files in the current working directory before searching the texmf tree

--- src/FileFinder.cpp
+++ src/FileFinder.cpp
@@ -132,6 +132,12 @@ static const char* find_file (const std::string &fname, const char *ftype) {
 	if (!_initialized || fname.empty())
 		return 0;
 
+	static std::string buf;
+	// try to lookup the file in the current working directory
+	buf = FileSystem::getcwd()+"/"+fname;
+	if (FileSystem::exists(buf.c_str()))
+		return buf.c_str();
+
 	std::string ext;
 	if (ftype)
 		ext = ftype;
@@ -142,7 +148,6 @@ static const char* find_file (const std::string &fname, const char *ftype) {
 		ext = fname.substr(pos+1);
 	}
 
-	static std::string buf;
 #ifdef MIKTEX
 	if (ext == "dll" || ext == "exe") {
 		// lookup dll and exe files in the MiKTeX bin directory first
