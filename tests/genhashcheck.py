9429ff37f73e244c3e3ea0f55850f6f4cb7e519e
Martin Gieseking <martin.gieseking@uos.de>
2016-01-18 20:02:09 +0100

    added test to check the validity of the char name hashes used in Unicode.cpp

---
+++ tests/genhashcheck.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python
+# This file is part of the dvisvgm package and published under the
+# terms of the GNU General Public License version 3 or later.
+# See file COPYING for further details.
+# Copyright (C) 2016 Martin Gieseking <martin.gieseking@uos.de>
+
+import re
+import sys
+
+def extract_hashes (fname):
+	with open(fname) as f:
+		lines = f.readlines()
+		found = False
+		for line in lines:
+			if line == '} hash2unicode[] = {\n':
+				found = True
+			elif found:
+				match = re.search(r'^\s*{(0x[0-9a-f]{8}),\s*0x[0-9a-f]{4}}, //\s*(.+)\s*$', line)
+				if match:
+					print '\t{%s, "%s"},' % (match.group(1), match.group(2))
+				else:
+					found = False
+
+if (len(sys.argv) < 2):
+	sys.exit(1)
+
+print """\
+#include <xxhash.h>
+#include <iomanip>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+struct NameHash {
+	unsigned hash;
+	string name;
+} nameHashes[] = {
+"""
+extract_hashes(sys.argv[1])
+print """\
+};
+
+int main () {
+	unsigned prev_hash=0;
+	for (unsigned i=0; i < sizeof(nameHashes)/sizeof(NameHash); i++) {
+		const string &name = nameHashes[i].name;
+		const unsigned hash = nameHashes[i].hash;
+		if (XXH32(&name[0], name.length(), 0) != hash) {
+			cout << "hash of '" << name << "' doesn't match\\n";
+			return 1;
+		}
+		if (hash < prev_hash) {
+			cout << "misplaced hash value " << hex << setw(8) << setfill('0') << hash << "\\n";
+			return 1;
+		}
+		if (hash == prev_hash) {
+			cout << "colliding hash values " << hex << setw(8) << setfill('0') << hash << "\\n";
+			return 1;
+		}
+		prev_hash = hash;
+	}
+	cout << "hash check passed\\n";
+	return 0;
+}
+"""
