80bc1f0ec18f6b40cbd4473fd902d64210d67f36
Martin Gieseking <martin.gieseking@uos.de>
2016-02-26 19:25:26 +0100

    set version to 1.15

--- vc/config.h
+++ vc/config.h
@@ -10,7 +10,7 @@
 #define PACKAGE_NAME "dvisvgm"
 
 /* Version number of package */
-#define VERSION "1.14.2"
+#define VERSION "1.15"
 
 /* Define to the full name and version of this package. */
 #define PACKAGE_STRING "dvisvgm " VERSION
