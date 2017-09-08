7cd82af7a984bd3fec46ad56e9accfd73c59853c
Martin Gieseking <martin.gieseking@uos.de>
2017-09-08 15:24:40 +0200

    define variable TEST_ARGV0 to make argv[0] of main() available in tests this is required to initialize kpathsea properly, for example

---
+++ tests/testmain.cpp
@@ -0,0 +1,30 @@
+/*************************************************************************
+** testmain.cpp                                                         **
+**                                                                      **
+** This file is part of dvisvgm -- a fast DVI to SVG converter          **
+** Copyright (C) 2005-2017 Martin Gieseking <martin.gieseking@uos.de>   **
+**                                                                      **
+** This program is free software; you can redistribute it and/or        **
+** modify it under the terms of the GNU General Public License as       **
+** published by the Free Software Foundation; either version 3 of       **
+** the License, or (at your option) any later version.                  **
+**                                                                      **
+** This program is distributed in the hope that it will be useful, but  **
+** WITHOUT ANY WARRANTY; without even the implied warranty of           **
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the         **
+** GNU General Public License for more details.                         **
+**                                                                      **
+** You should have received a copy of the GNU General Public License    **
+** along with this program; if not, see <http://www.gnu.org/licenses/>. **
+*************************************************************************/
+
+#include <string>
+#include "gtest/gtest.h"
+
+std::string TEST_ARGV0;
+
+GTEST_API_ int main (int argc, char **argv) {
+	TEST_ARGV0 = argv[0];
+	testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
+}
