170312bbaf6fd2a20c0986056a50d755aacf6e50
Martin Gieseking <martin.gieseking@uos.de>
2018-08-07 21:14:55 +0200

    set version to 2.5

--- src/version.hpp
+++ src/version.hpp
@@ -22,7 +22,7 @@
 #define VERSION_HPP
 
 constexpr const char *PROGRAM_NAME = "dvisvgm";
-constexpr const char *PROGRAM_VERSION = "2.4.2";
+constexpr const char *PROGRAM_VERSION = "2.5";
 
 #endif
 
