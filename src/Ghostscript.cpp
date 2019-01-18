e9a30bc6bead2e6b7ba945f85f8d9094b08b7687
Martin Gieseking <martin.gieseking@uos.de>
2019-01-18 14:34:22 +0100

    fixed formatting of GS version number

--- src/Ghostscript.cpp
+++ src/Ghostscript.cpp
@@ -237,8 +237,12 @@ int Ghostscript::revision () {
 /** Returns the revision of the GS library as a string of the form "MAJOR.MINOR". */
 string Ghostscript::revisionstr () {
 	string revstr;
-	if (int rev = revision())
-		revstr = to_string(rev/100) + "." + to_string(rev%100);
+	if (int rev = revision()) {
+		revstr = to_string(rev/100) + ".";
+		if (rev % 100 < 10)
+			revstr += "0";
+	  	revstr += to_string(rev%100);
+	}
 	return revstr;
 }
 
