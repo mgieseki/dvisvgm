2e7b7c1b7c66c1cae5c8e47424bc8f1a3ca9f517
Martin Gieseking <martin.gieseking@uos.de>
2019-10-18 11:37:14 +0200

    updated gtest to version 1.10

--- tests/testutil.hpp
+++ tests/testutil.hpp
@@ -1,18 +1,5 @@
 #pragma once
 
-namespace testing {
-	namespace internal {
-		enum GTestColor {
-			COLOR_DEFAULT,
-			COLOR_RED,
-			COLOR_GREEN,
-			COLOR_YELLOW
-		};
-
-		void ColoredPrintf(GTestColor color, const char* fmt, ...);
-	}
-}
-
 #define WARNING(...) \
 	ColoredPrintf(testing::internal::COLOR_YELLOW, "[ WARNING  ] "), \
 	ColoredPrintf(testing::internal::COLOR_YELLOW, __VA_ARGS__), \
