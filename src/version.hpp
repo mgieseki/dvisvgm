a5cbac6d75525fc09681583199fd07cc5b396a48
Martin Gieseking <martin.gieseking@uos.de>
2020-03-10 16:46:45 +0100

    set version to 2.9

--- src/version.hpp
+++ src/version.hpp
@@ -22,7 +22,7 @@
 #define VERSION_HPP
 
 constexpr const char *PROGRAM_NAME = "dvisvgm";
-constexpr const char *PROGRAM_VERSION = "2.8.2";
+constexpr const char *PROGRAM_VERSION = "2.9";
 
 #endif
 
