598446ac8353fd21cfd39749630b555b8b4cee1b
Martin Gieseking <martin.gieseking@uos.de>
2023-01-30 08:27:04 +0100

    set version to 3.0.2

--- src/version.hpp
+++ src/version.hpp
@@ -22,7 +22,7 @@
 #define VERSION_HPP
 
 constexpr const char *PROGRAM_NAME = "dvisvgm";
-constexpr const char *PROGRAM_VERSION = "3.0.1";
+constexpr const char *PROGRAM_VERSION = "3.0.2";
 
 #endif
 
